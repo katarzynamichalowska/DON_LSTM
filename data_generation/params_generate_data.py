920cd2f44003d3b11c8595f842096bcfa6124eb6
Jeffrey Lu <jeffrey.yflu@gmail.com>
2024-09-10 10:35:04 +0200

    Remove logic from params_generate_data

--- data_generation/params_generate_data.py
+++ data_generation/params_generate_data.py
@@ -1,13 +1,8 @@
-import os
-import numpy as np
-from data_generation import grid, difference_matrices
-
 seed = 9
 
-nr_realizations = 1000
+nr_realizations = 200
 folder = "./data"
 filename = "data_kdv.npz"
-filename = os.path.join(folder, filename)
 
 # Parameters
 x_max, x_points = 10, 100                   # The length (width/period) and number of points in the spatial domain
@@ -21,12 +16,3 @@ mu = -0.00001
 
 # Parameters (KdV)
 gamma, eta = 1., 6.
-
-# Make spatial and temporal grids
-x, dx = grid(x_max, x_points)
-t, dt = grid(t_max+dt, t_points+1)          # Added dt and 1 to have t_points in g_u
-
-D1, D2 = difference_matrices(x_max, x_points)
-
-g = lambda x, t: 0
-I = np.eye(x_points)
